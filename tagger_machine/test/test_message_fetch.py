df72a8f94779c2f259cac0cae1586831ee3ba987
ilya shusterman <ilya.shusterman@superfly.com>
2018-01-17 18:09:22 +0200

    minor code fix

--- tagger_machine/test/test_message_fetch.py
+++ tagger_machine/test/test_message_fetch.py
@@ -14,11 +14,12 @@ class TestMessageFetch(TestCase):
                                           offset=1)) == 50)
 
     def test_offset_messages_from_database(self):
+        dummy_amount = 2
         message_offset_one = \
-            self.message_fetcher.load_messages(messages_amount=2,
+            self.message_fetcher.load_messages(messages_amount=dummy_amount,
                                                offset=1)[1]
         message_offset_two = \
-            self.message_fetcher.load_messages(messages_amount=2,
+            self.message_fetcher.load_messages(messages_amount=dummy_amount,
                                                offset=2)[0]
         self.assertEqual(message_offset_one['id'], message_offset_two['id'])
 
