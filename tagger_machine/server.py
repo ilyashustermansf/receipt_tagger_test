6aa27f9600db218430c38b185f7213a6a0b5ac19
ilya shusterman <ilya.shusterman@superfly.com>
2018-01-17 03:18:21 +0200

    added todo for refactoring tornado

--- tagger_machine/server.py
+++ tagger_machine/server.py
@@ -3,6 +3,7 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 
+# TODO change this to tornado to handle wide requests
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return send_from_directory('web_client', 'index.html')
