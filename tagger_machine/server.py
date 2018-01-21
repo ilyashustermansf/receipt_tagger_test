7bd45053988377a8d1794544a23dd74a523aead6
ilya shusterman <shusterilyaman@gmail.com>
2018-01-21 18:19:59 +0200

    added server routing with add tags and get messages , need to finish testing

--- tagger_machine/server.py
+++ tagger_machine/server.py
@@ -1,6 +1,11 @@
+import json
+import logging
 import os
 import tornado.ioloop
 import tornado.web
+from tornado_json import schema
+
+from message_tag_handler import MessagesTagHandler
 
 CLIENT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                            'web_client'))
@@ -21,19 +26,46 @@ class MainHandler(tornado.web.RequestHandler):
             self.write(file.read())
 
 
-class MessageFetchHandler(tornado.web.RequestHandler):
+class MessageHandler(tornado.web.RequestHandler):
+
+    @classmethod
+    def setup(cls):
+        logging.basicConfig(level=logging.WARNING)
+        logger = logging.getLogger('tagger_machine')
+        logger.propagate = False
+        logger.setLevel(logging.WARNING)
+        handler = logging.StreamHandler()
+        logger.addHandler(handler)
+        return logger
+
+    def initialize(self, message_tag_table):
+        self.message_tag_handler = message_tag_table
+
+
+class MessageFetchHandler(MessageHandler):
 
     def get(self):
-        pass
+        # messages = self.message_tag_handler.get_next_messages()
+        self.write(json.dumps([{'id': 1234}, {'id': 1235}]))
+
+
+class AddTagsHandler(MessageHandler):
+
+    def post(self):
+        tags = []
+        self.message_tag_handler.add_tags(tags)
 
 
 def make_app():
+    MessageHandler.setup()
+    message_tag_table = dict(message_tag_table=
+                             MessagesTagHandler(messages_limit=50))
     handlers = [
         (r'/static_files/(.*)', tornado.web.StaticFileHandler,
          {'path': CLIENT_STATIC}),
         (r'/', MainHandler),
-        (r'/get_messages', MessageFetchHandler),
-
+        (r'/get_messages', MessageFetchHandler, message_tag_table),
+        (r'/add_tags', AddTagsHandler, message_tag_table),
     ]
     return tornado.web.Application(handlers)
 
